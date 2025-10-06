34c40700b7d3a7dec1bfc16be6dec388074933fc
Tiarnangormley <gormley-t5@ulster.ac.uk>
2025-10-06 16:08:04 +0100

    Added a constructer to the Book Class.

--- Library/Book.cs
+++ Library/Book.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Library
 {
-    internal class Book
+     class Book
     {
         string Title;
         string Author;
         string ISBN;
+
+        //Example of a constructor that allows us to construst a new Book Object
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+        }
 
         void DisplayInfo()
         {
@@ -22,24 +31,10 @@ namespace Library
 
         static void Main(string[] args)
         {
-            Book myBook = new Book(); //Creates new instance of book
-            myBook.Title = "C# for beginngers";
-            myBook.Author = "Bill Gates";
-            myBook.ISBN = "1234";
-
-            // ADD ANOTHER BOOK
-            //C# Mehthods
-            // Microsoft
-            // 3456778
-
-            Book myBook2 = new Book();
-            myBook2.Title = "C# Methods";
-            myBook2.Author = "Microsoft";
-            myBook2.ISBN = "3456778";
-
+            Book myBook = new Book("C# for beginners", "Bill Gates", "1234567" ); //Creates new instance of book
+           
             //Output book information to the console
             myBook.DisplayInfo();
-            myBook2.DisplayInfo();
         }
 
     }
